e1be9adc745c366e385e50cca702c30dd2b5bc3b
Семья <oc83@mail.ru>
2023-09-05 13:36:59 +0300

    Update main.py

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,21 +2,19 @@ from src.item import Item
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
-
     # длина наименования товара меньше 10 символов
     item.name = 'Смартфон'
     assert item.name == 'Смартфон'
-
     # длина наименования товара больше 10 символов
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
 
-    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+    Item.all = []
+    Item.instantiate_from_csv()  # создание объектов из данных файла
     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
+    print(item1.name)
     assert item1.name == 'Смартфон'
 
     assert Item.string_to_number('5') == 5
-    assert Item.string_to_number('5.0') == 5
-    assert Item.string_to_number('5.5') == 5
--- src/item.py
+++ src/item.py
@@ -1,32 +1,53 @@
+import csv
+
+
 class Item:
     """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
-    all = []
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-        :param name: Название товара.
+        Класс для представления товара в магазине.
+        @@ -13,18 +16,44 @@ def __init__(self, name: str, price: float, quantity: int) -> None:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+
+    Item.all.append(self)
+    self.__name = name
+    self.price = price
+    self.quantity = quantity
+
+    @staticmethod
+    def string_to_number(str_number):
+        return int(float(str_number))
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='', encoding='UTF-8') as file:
+            data = csv.DictReader(file)
+            for row in data:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) <= 10:
+            self.__name = new_name
+        else:
+            self.__name = new_name[:10]
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
         :return: Общая стоимость товара.
         """
-        pass
+
         return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+
         self.price *= self.pay_rate
