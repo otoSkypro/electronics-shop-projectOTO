a118b131e9d4212f6acffac494e9980390261ca9
Семья <oc83@mail.ru>
2023-09-22 19:29:16 +0300

    Update Item and Test_item

--- src/phone.py
+++ src/phone.py
@@ -30,22 +30,25 @@
 #         return (f"{self.__class__.__name__}(title='{self.title}', prise={self.prise}, quantity={self.quantity}, "
 #                 f"number_of_sim={self.number_of_sim})")
 #
-#     # def __repr__(self, ) -> str:
-#     #     return (f"{self.__class__.__name__}(name='{self.name}', price={self.price}, quantity={self.quantity}, "
-#     #             f"number_of_sim={self.number_of_sim})")
+#
 from src.item import Item
 
 
 class Phone(Item):
-
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int = 0, number_of_sim: int = 1) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        if not isinstance(number_of_sim, int) or number_of_sim < 1:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self.__number_of_sim = number_of_sim
 
-    def __add__(self, other):
-        if isinstance(other, (Phone, Item)):
-            return self.quantity + other.quantity
-        raise TypeError("Unsupported operand type(s) for +: {} and {}".format(type(self), type(other)))
 
-    def __repr__(self):
-        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int):
+        if not isinstance(value, int) or value < 1:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self.__number_of_sim = value
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,9 +1,9 @@
+from src import phone
 from src.phone import Phone
 from src.item import Item
 
 
 def test_phone_inheritance():
-
     phone = Phone("Смартфон", 10000, 20, 2)
     assert isinstance(phone, Item)
     assert hasattr(phone, 'name')
@@ -11,22 +11,17 @@ def test_phone_inheritance():
     assert hasattr(phone, 'quantity')
     assert hasattr(phone, 'number_of_sim')
 
+def test_number_of_sim():
+    assert isinstance(test_number_of_sim(), Phone)
 
-def test_add_phone_and_item():
-
-    phone = Phone("Смартфон", 10000, 20, 2)
-    item = Item("Ноутбук", 20000, 5)
-    result = phone + item
-    assert result == 25
+def test_raises_exception():
+    assert isinstance(test_raises_exception)
 
 
-def test_add_phone_and_phone():
 
-    phone1 = Phone("Смартфон 1", 10000, 10, 1)
-    phone2 = Phone("Смартфон 2", 12000, 15, 2)
-    result = phone1 + phone2
-    assert result == 25
 
+def test_is_phone_inheritance():
+    assert isinstance(isinstance(phone))
 
 # def test_add_phone_and_non_item_or_phone():
 #
